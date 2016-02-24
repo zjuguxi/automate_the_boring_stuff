9842db1bb936e952093c085ad2c77d60ff62c3d2
Alien <zjuguxi@gmail.com>
2016-02-24 16:50:35 +0800

    copied code

--- chapter_8_project1.py
+++ chapter_8_project1.py
@@ -3,6 +3,16 @@
 
 import random
 
-answers = {'q1': 'a1', 'q2': 'a2', 'q3': 'a3', 'q4': 'a4'}
+capitals = {'q1': 'a1', 'q2': 'a2', 'q3': 'a3', 'q4': 'a4'}
 
 for quizNum in range(4):
+    quizFile = open('capitalsquiz%s.txt' % (quizNum + 1), 'w')
+    answerFile = open('capitalsquiz_answer%s.txt' % (quizNum + 1), 'w')
+
+    quizFile.write('Name:\n\nData:\n\nPeriod:\n\n')
+    quizFile.write(('  '* 20) + 'State Capitals Quiz (Form %s)' % (quizNum + 1))
+    quizFile.write('\n\n')
+
+    states = list(capitals.keys())
+    random.shuffle(state)
+    
