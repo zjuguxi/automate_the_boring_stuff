5cb7277394e139a9e35d36e80723e3df30f6d444
Alien <zjuguxi@gmail.com>
2016-02-25 15:58:49 +0800

    debugged

--- chapter_8_project1.py
+++ chapter_8_project1.py
@@ -7,7 +7,7 @@ capitals = {'q1': 'a1', 'q2': 'a2', 'q3': 'a3', 'q4': 'a4'}
 
 for quizNum in range(4):
     quizFile = open('capitalsquiz%s.txt' % (quizNum + 1), 'w')
-    answerFile = open('capitalsquiz_answer%s.txt' % (quizNum + 1), 'w')
+    answerKeyFile = open('capitalsquiz_answer%s.txt' % (quizNum + 1), 'w')
 
     quizFile.write('Name:\n\nData:\n\nPeriod:\n\n')
     quizFile.write(('  '* 20) + 'State Capitals Quiz (Form %s)' % (quizNum + 1))
@@ -34,6 +34,6 @@ for quizNum in range(4):
 
             answerKeyFile.write('%s. %s\n' % (questionNum + 1, 'ABCD'[answerOptions.index(correctAnswer)]))
 
-        quizFile.close()
+    quizFile.close()
 
-        answerKeyFile.close()
+    answerKeyFile.close()
