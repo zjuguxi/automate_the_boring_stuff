b4719c40ff8bac4814a13bae9b2a2d78db26732c
Alien <zjuguxi@gmail.com>
2016-02-26 16:51:39 +0800

    chapter 9 finished

--- chapter_9_piece1.py
+++ chapter_9_piece1.py
@@ -3,9 +3,22 @@
 
 import os
 
-path = '/Users/apple/automate_the_boring_stuff/'
+# path = '/Users/apple/automate_the_boring_stuff/'
 
-for filename in os.listdir(path):
-    if filename.endswith('md'):
-        print(filename)
+# for filename in os.listdir(path):
+#     if filename.endswith('md'):
+#         print(filename)
 
+
+path = ('/Users/apple/')
+
+for foldername, subfolders, filenames in os.walk(path):
+    print('The current folder is ' + foldername)
+
+    for subfolder in subfolders:
+        print('Subfolder of ' + foldername +': ' + subfolder)
+
+    for filename in filenames:
+        print('File inside ' + foldername + ': ' + filename)
+
+    print(' ')
